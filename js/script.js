5fc6b375ea79cad8d2b3542950226a38b84d70f5
alexander.chernyshev <alexander.chernyshev@corp.mail.ru>
2020-12-18 12:01:11 +0300

    1 lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    mivies: {},
+    actions: {},
+    genries: [],
+    privat: false,
+};
+
+for (let i=0; i<2; i++) {
+    let lastFilmview = prompt('Один из последних просмотренных фильмов?', ''),
+        filmrating = prompt('На сколько оцените его?', '');
+    personalMovieDB.mivies[lastFilmview] = filmrating;
+}
+console.log(personalMovieDB);
