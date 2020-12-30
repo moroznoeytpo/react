2bf2fea4f7d6deef5f67fe1622cbe09f91926c7c
alexander.chernyshev <alexander.chernyshev@corp.mail.ru>
2020-12-30 15:28:03 +0300

    018

--- js/script.js
+++ js/script.js
@@ -1,18 +1,62 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     mivies: {},
     actions: {},
-    genries: [],
+    genres: [],
     privat: false,
 };
 
-for (let i=0; i<2; i++) {
-    let lastFilmview = prompt('Один из последних просмотренных фильмов?', ''),
-        filmrating = prompt('На сколько оцените его?', '');
-    personalMovieDB.mivies[lastFilmview] = filmrating;
+function start() {
+    let numberOfFilms;
+    while (!numberOfFilms || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+    personalMovieDB.count = numberOfFilms;
 }
-console.log(personalMovieDB);
+
+function rememberMyFilms(count) {
+    for (let i=0; i<count; i++) {
+        let lastFilmview, filmRating;
+        while (!lastFilmview || lastFilmview.length > 50 || !filmRating) {
+            lastFilmview = prompt('Один из последних просмотренных фильмов?', '');
+            filmRating = prompt('На сколько оцените его?', '');
+        }
+        personalMovieDB.mivies[lastFilmview] = filmRating;
+    }
+}
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log("Вы классный зритель");
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres(count) {
+    for (let i = 0; i < count; i++) {
+        let genre;
+        while (!genre) {
+            genre = prompt(`Ваш любимый жанр под номером ${i+1}`, '');
+        }
+        personalMovieDB.genres.push(genre);
+    }
+}
+
+start();
+rememberMyFilms(2);
+detectPersonalLevel();
+writeYourGenres(3);
+showMyDB(personalMovieDB.privat);
